5838ddf56363b2578e26697e414682e986caa29b
Jean Paul R <71387616+killua156@users.noreply.github.com>
2025-02-19 17:29:02 -0600

    codeChanges

---
+++ Web Scarping/webScraping_Beginner.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://quotes.toscrape.com/"
+response = requests.get(url)
+
+# Parse the HTML
+soup = BeautifulSoup(response.text, "html.parser")
+
+# Extract quotes
+quotes = soup.find_all("span", class_="text")
+
+for quote in quotes:
+    print(quote.text)
