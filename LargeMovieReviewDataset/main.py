b6b68eadf1649eb9d5cfa316860d3896d2c09331
Jean <hackert156@gmail.com>
2025-02-22 02:21:19 -0600

    INTER

--- LargeMovieReviewDataset/main.py
+++ LargeMovieReviewDataset/main.py
@@ -4,6 +4,9 @@ import pandas as pd
 # Define folder paths relative to the script
 neg_path = r"C:\Users\jeanr\OneDrive\Desktop\Machine Learning\Machine-Learning\LargeMovieReviewDataset\train\neg"
 pos_path = r"C:\Users\jeanr\OneDrive\Desktop\Machine Learning\Machine-Learning\LargeMovieReviewDataset\train\pos"
+neg_test_path = r"C:\Users\jeanr\OneDrive\Desktop\Machine Learning\Machine-Learning\LargeMovieReviewDataset\test\neg"
+pos_test_path = r"C:\Users\jeanr\OneDrive\Desktop\Machine Learning\Machine-Learning\LargeMovieReviewDataset\test\pos"
+
 
 
 # Function to read all text files in a folder
@@ -27,3 +30,17 @@ csv_path = "./reviews.csv"
 df.to_csv(csv_path, index=False)
 
 print(f"CSV file '{csv_path}' created successfully!")
+
+# Load negative and positive test reviews
+neg_reviews = load_reviews(neg_test_path, "negative")
+pos_reviews = load_reviews(pos_test_path, "positive")
+
+# Combine and create DataFrame
+df_test = pd.DataFrame(neg_reviews + pos_reviews, columns=["review", "label"])
+
+# Save to CSV in the current directory
+csv_test_path = "./reviewsTesting.csv"
+df_test.to_csv(csv_test_path, index=False)
+
+print(f"CSV file '{csv_test_path}' created successfully!")
+
